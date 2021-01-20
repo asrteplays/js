d7b0757e42a759313ea140f0520a543372a18bd8
Aleksei <68428066+asrteplays@users.noreply.github.com>
2021-01-20 15:16:11 +0200

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+// window.addEventListener('DOMContentLoaded', function(){
+//   document.querySelectorAll('.text').forEach(function(domElement) {
+//     domElement.classList.add('alert')
+//   })
+// })
+
+
+// window.addEventListener('DOMContentLoaded', function(){
+//   document.querySelector('#burger').addEventListener('click', function(){
+//     document.querySelector('#menu').classList.toggle('is-active')
+//   })
+
+//   document.querySelectorAll('.text').forEach(function(elementDOM){
+//     elementDOM.addEventListener('click', function(event){
+//       event.target.classList.toggle('alert')
+//     })
+//   })
+// })
